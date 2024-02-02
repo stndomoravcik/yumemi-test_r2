19d43c13ad49d7519e3485339df64bb525ffceab
Stanislav Moravcik <stndomoravcik@gmail.com>
2024-02-03 00:44:11 +0900

    UIブラッシュアップ/Fat VC対策（起動画面をSwiftUIで実装＋必要な設定を変更

---
+++ iOSEngineerCodeCheck/SearchView.swift
@@ -0,0 +1,16 @@
+//
+//  SearchView.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by Moravcik Stanislav on 2024/02/03.
+//  Copyright © 2024 YUMEMI Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    
+    var body: some View {
+        Text("test")
+    }
+}
