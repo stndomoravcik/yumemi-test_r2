0823d4db6bcaadaa12f7d9d199735f04171183a2
Stanislav Moravcik <stndomoravcik@gmail.com>
2024-02-05 00:03:48 +0900

    Resultを使うようにすることで必要に応じてアラート（データ取得失敗など）を表示

--- iOSEngineerCodeCheck/Services/APIManager.swift
+++ iOSEngineerCodeCheck/Services/APIManager.swift
@@ -8,21 +8,41 @@
 
 import Foundation
 
+enum APIError: Error {
+    case noData
+    case serialization
+    case other(String)
+    
+    var errorMessage: String {
+        switch self {
+        case .noData:
+            "Data not available"
+        case .serialization:
+            "Serialization Failure"
+        case .other(let string):
+            string
+        }
+    }
+}
+
 class APIManager {
     
     static let shared = APIManager()
     
     private var downloadTask: URLSessionTask?
     
-    private func downloadData(_ url: URL, completion: @escaping (Data?) -> Void) {
+    private func downloadData(_ url: URL, completion: @escaping (Result<Data, APIError>) -> Void) {
         
         downloadTask = URLSession.shared.dataTask(with: url) { (data, res, err) in
             if let error = err {
-                print("Error: \(error.localizedDescription)")
-                completion(nil)
+                completion(.failure(.other(error.localizedDescription)))
                 return
             }
-            completion(data)
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            completion(.success(data))
         }
         downloadTask?.resume()
     }
@@ -31,35 +51,36 @@ class APIManager {
         downloadTask?.cancel()
     }
     
-    func searchGitRepositories(query: String, completion: @escaping ([Repo]) -> Void) {
+    func searchGitRepositories(query: String, completion: @escaping (Result<[Repo], APIError>) -> Void) {
         guard let searchURL = URL(string: "https://api.github.com/search/repositories?q=\(query)") else {
             return
         }
-        downloadData(searchURL) { data in
-            guard let data = data else {
-                completion([])
-                return
-            }
-            do {
-                guard let json = try JSONSerialization.jsonObject(with: data) as? [String : Any] else {
-                    completion([])
-                    return
-                }
-                if let items = json["items"] as? [[String: Any]] {
-                    let repos = items.map { item in
-                        Repo(item)
+        downloadData(searchURL) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    guard let json = try JSONSerialization.jsonObject(with: data) as? [String : Any] else {
+                        completion(.failure(.other("Wrong type")))
+                        return
                     }
-                    completion(repos)
-                } else {
-                    completion([])
+                    if let items = json["items"] as? [[String: Any]] {
+                        let repos = items.map { item in
+                            Repo(item)
+                        }
+                        completion(.success(repos))
+                    } else {
+                        completion(.failure(.other("Missing items key")))
+                    }
+                } catch {
+                    completion(.failure(.serialization))
                 }
-            } catch {
-                completion([])
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
     }
     
-    func getRepositoryImageData(_ imageURL: String, completion: @escaping (Data?) -> Void){
+    func getRepositoryImageData(_ imageURL: String, completion: @escaping (Result<Data, APIError>) -> Void){
         guard let url = URL(string: imageURL) else { return }
         downloadData(url, completion: completion)
     }
