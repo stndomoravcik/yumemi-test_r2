86270597d4e3b2f138d672f7c1d3ae0d6d2dd97b
Stanislav Moravcik <stndomoravcik@gmail.com>
2024-02-05 20:46:33 +0900

    簡単な単体テストを追加

---
+++ iOSEngineerCodeCheckTests/APIManagerTests.swift
@@ -0,0 +1,57 @@
+//
+//  APIManagerTests.swift
+//  iOSEngineerCodeCheckTests
+//
+//  Created by Moravcik Stanislav on 2024/02/05.
+//  Copyright © 2024 YUMEMI Inc. All rights reserved.
+//
+
+import XCTest
+@testable import iOSEngineerCodeCheck
+
+class APIManagerTests: XCTestCase {
+    private let apiManager = APIManager.shared
+    
+    private func getRepo(_ name: String, completion: @escaping (Repo?) -> Void) {
+        apiManager.searchGitRepositories(query: "yumemi") { result in
+            switch result {
+            case .success(let repo):
+                completion(repo.first)
+            case .failure(let failure):
+                completion(nil)
+            }
+        }
+    }
+    
+    func testFetchGitRepository() {
+        let expectation = XCTestExpectation(description: "Fetch repositories")
+        getRepo("yumemi") { result in
+            if result == nil {
+                XCTFail("Repo is nil")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 3.0)
+    }
+    
+    func testGetRepositoryImageData() {
+        let expectation = XCTestExpectation(description: "Fetch repositories")
+        getRepo("yumemi") { [weak self] result in
+            if let url = result?.avatarUrl {
+                self?.apiManager.getRepositoryImageData(url) { data in
+                    switch data {
+                    case .success(let data):
+                        let image = UIImage(data: data)
+                        XCTAssertNotNil(image)
+                    case .failure(let error):
+                        XCTFail("Failed with error: \(error.localizedDescription)")
+                    }
+                }
+            } else {
+                XCTFail("Image URL is nil")
+            }
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 3.0)
+    }
+}
